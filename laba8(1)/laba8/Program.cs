378223f4c16edbfae5361ec7a6249ad5383e55cf
KyrylBudanov <boris201393@gmail.com>
2023-11-28 00:06:50 +0200

    first commit

---
+++ laba8(1)/laba8/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+public class ConfigurationManager
+{
+    private static ConfigurationManager instance;
+    private string loggingMode;
+    private string databaseConnection;
+
+    // Приватний конструктор для запобігання створення екземпляра через конструктор
+    private ConfigurationManager() { }
+
+    // Метод для отримання єдиного екземпляру ConfigurationManager
+    public static ConfigurationManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new ConfigurationManager();
+            }
+            return instance;
+        }
+    }
+
+    // Метод для встановлення параметрів конфігурації
+    public void SetConfiguration(string loggingMode, string databaseConnection)
+    {
+        this.loggingMode = loggingMode;
+        this.databaseConnection = databaseConnection;
+    }
+
+    // Метод для виведення параметрів конфігурації
+    public void DisplayConfiguration()
+    {
+        Console.WriteLine($"Logging Mode: {loggingMode}");
+        Console.WriteLine($"Database Connection: {databaseConnection}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Отримання єдиного екземпляру ConfigurationManager
+        ConfigurationManager configManager = ConfigurationManager.Instance;
+
+        // Встановлення конфігурації через консольний інтерфейс
+        Console.Write("Enter logging mode: ");
+        string loggingMode = Console.ReadLine();
+
+        Console.Write("Enter database connection: ");
+        string databaseConnection = Console.ReadLine();
+
+        // Встановлення конфігурації
+        configManager.SetConfiguration(loggingMode, databaseConnection);
+
+        // Виведення конфігурації
+        Console.WriteLine("\nCurrent Configuration:");
+        configManager.DisplayConfiguration();
+
+        // Додаткові операції або використання конфігурації можуть бути додані за потребою
+    }
+}
---
+++ laba8(4)/laba8/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+// Прототип для шаблонів даних
+[DataContract]
+public class DataTemplate : ICloneable
+{
+    [DataMember]
+    public string Field1 { get; set; }
+
+    [DataMember]
+    public string Field2 { get; set; }
+
+    public object Clone()
+    {
+        // Використовуємо глибоке клонування для копіювання об'єкта
+        using (var stream = new MemoryStream())
+        {
+            var serializer = new DataContractJsonSerializer(typeof(DataTemplate));
+            serializer.WriteObject(stream, this);
+            stream.Seek(0, SeekOrigin.Begin);
+            return serializer.ReadObject(stream);
+        }
+    }
+}
+
+// Адаптер для забезпечення сумісності між форматами
+public interface IDataAdapter
+{
+    string ConvertData(object data);
+}
+
+// Адаптер для конвертації в JSON
+public class JsonDataAdapter : IDataAdapter
+{
+    public string ConvertData(object data)
+    {
+        using (var stream = new MemoryStream())
+        {
+            var serializer = new DataContractJsonSerializer(data.GetType());
+            serializer.WriteObject(stream, data);
+            stream.Seek(0, SeekOrigin.Begin);
+            using (var reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
+
+// Адаптер для конвертації в XML
+public class XmlDataAdapter : IDataAdapter
+{
+    public string ConvertData(object data)
+    {
+        using (var stream = new MemoryStream())
+        {
+            var serializer = new XmlSerializer(data.GetType());
+            serializer.Serialize(stream, data);
+            stream.Seek(0, SeekOrigin.Begin);
+            using (var reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
+
+// Система імпорту та експорту
+public class DataConverter
+{
+    private readonly Dictionary<string, IDataAdapter> adapters;
+
+    public DataConverter()
+    {
+        adapters = new Dictionary<string, IDataAdapter>
+        {
+            {"json", new JsonDataAdapter()},
+            {"xml", new XmlDataAdapter()}
+            // Додайте адаптери для інших форматів за необхідності
+        };
+    }
+
+    public string ConvertData(object data, string sourceFormat, string targetFormat)
+    {
+        if (adapters.TryGetValue(sourceFormat.ToLower(), out var sourceAdapter) &&
+            adapters.TryGetValue(targetFormat.ToLower(), out var targetAdapter))
+        {
+            // Конвертація даних з одного формату в інший через проміжний об'єкт
+            var intermediateData = sourceAdapter.ConvertData(data);
+            return targetAdapter.ConvertData(intermediateData);
+        }
+
+        return "Невідомий формат адаптера";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        DataConverter dataConverter = new DataConverter();
+
+        Console.WriteLine("Введіть дані для конвертації:");
+        Console.Write("Field1: ");
+        string field1 = Console.ReadLine();
+        Console.Write("Field2: ");
+        string field2 = Console.ReadLine();
+
+        DataTemplate dataTemplate = new DataTemplate { Field1 = field1, Field2 = field2 };
+
+        Console.WriteLine("Введіть формат вихідних даних (json/xml):");
+        string sourceFormat = Console.ReadLine();
+
+        Console.WriteLine("Введіть формат цільових даних (json/xml):");
+        string targetFormat = Console.ReadLine();
+
+        string result = dataConverter.ConvertData(dataTemplate, sourceFormat, targetFormat);
+        Console.WriteLine("Результат конвертації:");
+        Console.WriteLine(result);
+    }
+}
